00f4ff6f28412dd8b4ad0def539b747cfb18df93
JMarshall <13102167578@163.com>
2019-07-20 23:26:24 +0800

    我起了，一下抄完了，有什么好说的

--- SwiftTEST/ViewController.swift
+++ SwiftTEST/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//
+//        let str = "{\"result\": \"胖大舌:否|点刺:无|裂纹:无|齿痕:无|舌色:青紫|薄瘦:无|苔色:黄|舌苔厚薄:少苔|润燥:燥|腻腐:腐|剥落:无|\"}"
+//
+//        print(str);
+//        let subString = str.split(separator: "\"")
+//        print(subString[3])
+//        let sickInfoSet = subString[3].split(separator: "|");
+//        print(sickInfoSet,sickInfoSet.count)
+
+        let jeremyGif = UIImage.gifImageWithName("loading")
+        imageView.image = jeremyGif;
+        
+        print("sdasdas")
     }
 
 
